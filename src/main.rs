06e7906103a7026dacb548f61be7fc44950d0cd0
Flurin Feuerstein <ffeuerstein@hamilton.ch>
2022-09-28 14:48:12 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,106 @@
+mod models;
+
+use dotenv::dotenv;
+use models::{PullRequest, PullRequestList, Repository, RepositoryList};
+use reqwest::Client;
+
+const PASSWORD_KEY: &str = "PAT";
+const USER_KEY: &str = "USER";
+const URL_KEY: &str = "URL";
+const VERSION: &str = "?api-version=7.1-preview.1";
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv()?;
+    let password = dotenv::var(PASSWORD_KEY)?;
+
+    let user = dotenv::var(USER_KEY)?;
+    let url = dotenv::var(URL_KEY)?;
+
+    let azure = Azure::new("", &password, &url);
+    let repositories = azure.get_repositories().await?;
+
+    let mut relevant_pull_requests: Vec<PullRequest> = vec![];
+
+    println!("Getting open Pull Requests for user");
+
+    for repository in repositories {
+        print!(".");
+        let pull_requests = azure.get_pull_requests(&repository).await?;
+
+        let pull_requests: Vec<PullRequest> = pull_requests
+            .into_iter()
+            .filter(|x| x.createdBy.displayName == user)
+            .collect();
+
+        for pull_request in pull_requests {
+            relevant_pull_requests.push(pull_request);
+        }
+    }
+
+    println!(".");
+
+    for pull_request in relevant_pull_requests {
+        println!(
+            "PR \"{}\" | Repository \"{}\"",
+            pull_request.title, pull_request.repository.name
+        );
+    }
+
+    Ok(())
+}
+
+struct Azure {
+    url: String,
+    username: String,
+    password: String,
+    client: Client,
+}
+
+impl Azure {
+    pub fn new(username: &str, password: &str, url: &str) -> Self {
+        Self {
+            username: username.to_owned(),
+            password: password.to_owned(),
+            url: url.to_owned(),
+            client: Client::new(),
+        }
+    }
+
+    pub async fn get_repositories(&self) -> Result<Vec<Repository>, Box<dyn std::error::Error>> {
+        let url = format!("{}/_apis/git/repositories{VERSION}", self.url);
+
+        let response = self
+            .client
+            .get(url)
+            .basic_auth(&self.username, Some(&self.password))
+            .send()
+            .await?;
+
+        let repository_list = response.json::<RepositoryList>().await?;
+
+        Ok(repository_list.repositories)
+    }
+
+    pub async fn get_pull_requests(
+        &self,
+        repository: &Repository,
+    ) -> Result<Vec<PullRequest>, Box<dyn std::error::Error>> {
+        let repo_id = repository.id;
+        let url = format!(
+            "{}/_apis/git/repositories/{repo_id}/pullrequests{VERSION}",
+            self.url
+        );
+
+        let response = self
+            .client
+            .get(url)
+            .basic_auth(&self.username, Some(&self.password))
+            .send()
+            .await?;
+
+        let pull_requests = response.json::<PullRequestList>().await?;
+
+        Ok(pull_requests.pull_requests)
+    }
+}
---
+++ src/models.rs
@@ -0,0 +1,134 @@
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct RepositoryList {
+    pub count: i32,
+    #[serde(rename = "value")]
+    pub repositories: Vec<Repository>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct PullRequestList {
+    pub count: i32,
+    #[serde(rename = "value")]
+    pub pull_requests: Vec<PullRequest>,
+}
+
+#[allow(non_snake_case)]
+#[derive(Debug, Deserialize)]
+pub struct PullRequest {
+    pub repository: PullRequestRepository,
+    pub pullRequestId: i32,
+    pub codeReviewId: i32,
+    pub status: String,
+    pub createdBy: User,
+    pub creationDate: String,
+    pub title: String,
+    pub sourceRefName: String,
+    pub targetRefName: String,
+    pub mergeStatus: String,
+    pub isDraft: bool,
+    pub mergeId: uuid::Uuid,
+    pub lastMergeSourceCommit: Commit,
+    pub lastMergeTargetCommit: Commit,
+    pub lastMergeCommit: Commit,
+    pub reviewers: Vec<Reviewer>,
+    pub url: String,
+    pub completionOptions: Option<CompletionOptions>,
+    pub supportsIterations: bool,
+    pub autoCompleteSetBy: Option<User>,
+}
+
+#[allow(non_snake_case, dead_code)]
+#[derive(Debug, Deserialize)]
+pub struct PullRequestRepository {
+    pub id: uuid::Uuid,
+    pub name: String,
+    pub url: String,
+    pub project: PullRequestProject,
+}
+
+#[allow(non_snake_case, dead_code)]
+#[derive(Debug, Deserialize)]
+pub struct PullRequestProject {
+    pub id: uuid::Uuid,
+    pub name: String,
+    pub state: String,
+    pub visibility: String,
+    pub lastUpdateTime: String,
+}
+
+#[allow(non_snake_case, dead_code)]
+#[derive(Debug, Deserialize)]
+pub struct User {
+    pub displayName: String,
+    pub url: String,
+    pub _links: Links,
+    pub id: uuid::Uuid,
+    pub uniqueName: String,
+    pub imageUrl: String,
+    pub descriptor: String,
+}
+
+#[allow(non_snake_case, dead_code)]
+#[derive(Debug, Deserialize)]
+pub struct Links {
+    pub avatar: Avatar,
+}
+
+#[allow(non_snake_case, dead_code)]
+#[derive(Debug, Deserialize)]
+pub struct Avatar {
+    pub href: String,
+}
+
+#[allow(non_snake_case, dead_code)]
+#[derive(Debug, Deserialize)]
+pub struct Reviewer {
+    pub reviewerUrl: String,
+    pub vote: i32,
+    pub hasDeclined: bool,
+    pub isFlagged: bool,
+    pub displayName: String,
+    pub url: String,
+    pub _links: Links,
+    pub id: uuid::Uuid,
+    pub uniqueName: String,
+    pub imageUrl: String,
+}
+
+#[allow(non_snake_case, dead_code)]
+#[derive(Debug, Deserialize)]
+pub struct Commit {
+    pub commitId: String,
+    pub url: String,
+}
+
+#[allow(non_snake_case, dead_code)]
+#[derive(Debug, Deserialize)]
+pub struct CompletionOptions {
+    pub mergeCommitMessage: String,
+    pub deleteSourceBranch: bool,
+    pub squashMerge: bool,
+    pub mergeStrategy: String,
+    pub transitionWorkItems: bool,
+    pub autoCompleteIgnoreConfigIds: Vec<i32>,
+}
+
+#[allow(non_snake_case)]
+#[derive(Debug, Deserialize)]
+pub struct Repository {
+    pub id: uuid::Uuid,
+    pub name: String,
+    pub url: String,
+    pub project: Project,
+    pub remoteUrl: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Project {
+    pub id: uuid::Uuid,
+    pub name: String,
+    pub url: String,
+    pub state: String,
+}
