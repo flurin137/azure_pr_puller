a96c9cc310599cb33b87b4fa867d4792027b6f8e
Feuerstein Flurin <ffeuerstein@hamilton.ch>
2025-03-13 10:40:43 +0100

    basically working :-D

--- puller-ui/src/PullRequests.svelte
+++ puller-ui/src/PullRequests.svelte
@@ -6,8 +6,28 @@
 </script>
 
 {#each pullRequests as pullRequest}
-<div class="flex flex-col bg-primary p-4">
-    <div>{pullRequest.status}</div>
-    <a href="{pullRequest.url}" target="_blank">Click Me</a>
-</div>
+    <a
+        href={pullRequest.url} target="_blank"
+        class="flex p-4 rounded gap-4 justify-between {pullRequest.isDraft
+            ? 'bg-secondary'
+            : 'bg-primary'}"
+    >
+        <div class="flex flex-col gap-1">
+            <div class="font-bold">{pullRequest.pullRequestId}</div>
+            <div>{pullRequest.title}</div>
+        </div>
+
+        <div class="flex flex-col items-end gap-1">
+            <div>{pullRequest.createdBy.displayName}</div>
+
+            <div class="flex gap-2">
+                {#if pullRequest.isDraft}
+                    <div class="badge badge-accent">draft</div>
+                {/if}
+                {#if pullRequest.mergeStatus != "succeeded"}
+                    <div class="badge badge-error">Merge Issues</div>
+                {/if}
+            </div>
+        </div>
+    </a>
 {/each}
